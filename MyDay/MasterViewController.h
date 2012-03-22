a614aa4ffb27e50ed460326c2fa04f9f92d9de64
Stas Sviridenko <stas.sviridenko@gmail.com>
2012-03-22 11:59:30 +0400

    Initial Commit

---
+++ MyDay/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  MyDay
+//
+//  Created by Stas Sviridenko on 22.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
